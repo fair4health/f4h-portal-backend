de2998193b9189f7631a502c5c9198b1fc9e055b
J. Mario Rguez <juan.rodriguezp@atos.net>
2020-06-05 10:34:52 +0200

    Add backend application initial commit

---
+++ src/main/java/eu/fair4health/f4hportalbackend/F4hPortalBackendApplication.java
@@ -0,0 +1,13 @@
+package eu.fair4health.f4hportalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F4hPortalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F4hPortalBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/fair4health/f4hportalbackend/F4hPortalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.fair4health.f4hportalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F4hPortalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
